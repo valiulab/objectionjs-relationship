dbdcd10936cd24e8beaa77ed0c9cb97e42c882fb
Rodrigo Alcorta <rodrigoalcorta@Rodrigos-MacBook-Air.local>
2022-05-30 21:37:35 -0300

    ADD: Cleaner interfaces

--- src/interfaces/relationships.interface.ts
+++ src/interfaces/relationships.interface.ts
@@ -4,26 +4,22 @@ export interface IObjectionModelRelationship<T extends Model> {
     [key: string]: IObjectionModelRelationshipSchema<T>,
 }
 
+export interface IObjectionModelRelationshipSchemaJoin {
+    from: string | string[],
+    through?: {
+        from?: string | string[];
+        to?: string | string[];
+    },
+    to: string | string[];
+}
+
 export interface IObjectionModelRelationshipSchema<T extends Model> {
     relation: RelationType;
     modelClass: ModelClass<T> | string;
-    join: {
-        from: string,
-        through?: {
-            from?: string;
-            to?: string;
-        },
-        to: string;
-    }
+    join: IObjectionModelRelationshipSchemaJoin
 }
 
-export interface IObjectionModelRelationshipAddConfig {
-    from?: string;
-    through?: {
-        from?: string;
-        to?: string;
-    }
-    to?: string;
+export interface IObjectionModelRelationshipAddConfig extends Partial<Omit<IObjectionModelRelationshipSchemaJoin, 'relation' | 'modelClass'>> {
     fromTable?: string;
     toTable?: string;
     fromField?: string;
